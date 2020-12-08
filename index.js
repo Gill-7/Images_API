48f8f1aa21a35b81fdcd8c4b9ccbedbf558f9592
Gillpreet <gillpreetsinghgill@gmail.com>
2020-12-07 19:31:30 -0800

    complete commit

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const setupDiv = document.getElementById('setup')
-const puchlineDiv = document.getElementById('puchline')
-const puchlineBtn = document.getElementById('puchlineBtn')
+const punchlineDiv = document.getElementById('punchline')
+const punchlineBtn = document.getElementById('punchlineBtn')
 const newJokeBtn = document.getElementById('newJokeBtn')
 
+let punchline;
 
-const getJoke = async () => {
+punchlineBtn.addEventListener('click', getPunchline)
+
+newJokeBtn.addEventListener('click', getJoke);
+
+
+function getPunchline() {
+    punchlineDiv.innerHTML = punchline;
+    punchlineDiv.classList.add('bubble');
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
+}
+
+async function getJoke() {
     const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random')
     const joke = await jokePromise.json();
 
-    console.log(joke);
+    setupDiv.innerHTML = joke[0].setup;
+    
+    punchline = joke[0].punchline;
+
+    punchlineDiv.innerHTML = '';
+    punchlineDiv.classList.remove('bubble');
+
+
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
+   
 }
 getJoke()
